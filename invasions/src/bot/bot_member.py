a21f983f3bc0ec2e7e1d244b6630837099946505
Chatz <david.chatterton@gmail.com>
2024-05-20 17:12:57 +1000

    Update member flag in existing invasions when adding new member

--- invasions/src/bot/bot_member.py
+++ invasions/src/bot/bot_member.py
@@ -3,7 +3,6 @@ from botocore.exceptions import ClientError
 from boto3.dynamodb.conditions import Key, Attr
 import os
 from datetime import datetime
-import urllib3
 
 # get bucket name of environment
 s3 = boto3.client('s3')
@@ -98,6 +97,45 @@ def register_member(player:str, day:int, month:int, year:int, faction:str, disco
     return f'# New member {player}\nFaction: {faction}\nStarting {start}\nAdmin {admin}'
 
 
+def update_invasions(player:str, day:int, month:int, year:int) -> str:
+    print(f'update_invasions: {player} {day} {month} {year}')
+
+    zero_month = '{0:02d}'.format(month)
+    zero_day = '{0:02d}'.format(day)
+    start = int(f'{year}{zero_month}{zero_day}')
+
+    response = table.query(KeyConditionExpression=Key('invasion').eq('#invasion'),
+                           ProjectionExpression='id, #d',
+                           FilterExpression=Attr('date').gte(start),
+                           ExpressionAttributeNames={'#d': 'date'})
+    print(f'Invasions on or after {start}: {response}')
+
+    if not response.get('Items', None):
+        mesg = f'\nNo invasions found on or after {start} to update\n'
+    else:
+        items = response["Items"]
+        mesg = f'\n## Member flag updated in these invasions:\n'
+        for i in items:
+            invasion = i["id"]
+            ladder = table.query(KeyConditionExpression=Key('invasion').eq(f'#ladder#{invasion}'),
+                                    ProjectionExpression='id, #n, #r',
+                                    FilterExpression=Attr('name').eq(player),
+                                    ExpressionAttributeNames={'#n': 'name', '#r': 'rank'})
+
+            if ladder.get('Items', None):
+                print(f'Ladder for invasion {invasion} with place for {player}: {ladder}')
+                rank = ladder["Items"][0]["id"]
+                mesg += f'- {invasion} rank {rank}\n'
+                update = table.update_item(Key={'invasion': f'#ladder#{invasion}', 'id': rank},
+                                           UpdateExpression='set #m = :m',
+                                           ExpressionAttributeNames={'#m': 'member'},
+                                           ExpressionAttributeValues={':m': True},
+                                           ReturnValues='UPDATED_NEW')
+                print(f'update_item: #ladder#{invasion} {rank} {update}')
+
+    return mesg
+
+
 def member_add(options:list) -> str:
     print(f'member add: {options}')
 
@@ -127,7 +165,7 @@ def member_add(options:list) -> str:
         elif o["name"] == "notes":
             notes = o["value"]
 
-    return register_member(player=player,
+    mesg = register_member(player=player,
                            day=day,
                            month=month,
                            year=year,
@@ -135,7 +173,13 @@ def member_add(options:list) -> str:
                            discord=discord,
                            admin=admin,
                            notes=notes)
+    
+    mesg += update_invasions(player=player,
+                             day=day,
+                             month=month,
+                             year=year)
 
+    return mesg
 
 def member_remove(options:list) -> str:
     print(f'member remove: {options}')
@@ -180,128 +224,3 @@ def member_cmd(options:dict, resolved: dict) -> str:
     except Exception as e:
         print(f'Member command error: {e}')
         return f'Member command error: {e}'
-
-# # define lambda handler that takes an invasion name and calls create_folder
-# def lambda_handler(event, context):
-
-#     body = ""
-#     statusCode = 200
-#     headers = {
-#         "Content-Type": "application/json"
-#     }
-
-#     print(event)
-
-#     try:
-#         # get current member list
-#         if event['httpMethod'] == 'GET' and event['resource'] == '/members':
-
-#             response = table.query(KeyConditionExpression=Key('invasion').eq('#member'),
-#                                    ProjectionExpression='id')
-#             # set body to id and labels
-#             print(response)
-        
-#             if not response.get('Items', None):
-#                 statusCode = 404
-#                 body = f'No members found'
-#             else:
-#                 items = response["Items"]
-#                 body = f"Members {len(items)}: "
-#                 if len(items) > 0:
-#                     body += items.pop(0)["id"]
-#                     for player in items:
-#                         body += ", " + player["id"]
-
-#         # if routeKey is GET /, set body to a list of all invasions from table
-#         elif event['httpMethod'] == 'GET' and event['resource'] == '/members/{player}':
-
-#             player = event['pathParameters']['player']
-#             response = table.get_item(Key={"invasion": "#member", "id": player})
-#             print(response)
-        
-#             if not 'Item' in response:
-#                 statusCode = 404
-#                 body = f'Member {player} not found'
-#             else:
-#                 body = str(response["Item"])
-
-#         elif event['httpMethod'] == 'POST' and event['resource'] == '/members/add':
-#             # add or update a member
-#             request = json.loads(event['body'])
-#             timestamp = datetime.today().strftime('%Y%m%d%H%M%S')
-
-#             player = request['player']
-#             faction = request['faction']
-#             discord = request['discord'] if 'discord' in request else None
-#             start = request['start'] if 'start' in request else datetime.today().strftime('%Y%m%d')
-#             notes = request['notes'] if 'notes' in event else None
-#             admin = request['admin'] == True if 'admin' in request else False
-
-#             additem = {
-#                 'invasion': '#memberevent',
-#                 'id': timestamp,
-#                 'event': "add",
-#                 'player': player,
-#                 'faction': faction,
-#                 'admin': admin
-#             }
-
-#             if discord:
-#                 additem['discord'] = discord
-#             if notes:
-#                 additem['notes'] = notes
-
-#             memberitem = {
-#                 'invasion': '#member',
-#                 'id': player,
-#                 'faction': faction,
-#                 'admin': admin,
-#                 'event': timestamp,
-#                 'start': start
-#             }
-
-#             if discord:
-#                 memberitem['discord'] = discord
-#             if notes:
-#                 memberitem['notes'] = notes
-
-#             # Add event for adding this member and update list of members
-#             table.put_item(Item=additem)
-#             table.put_item(Item=memberitem)
-#             body = f'Added member {player} faction {faction} starting {start} admin {admin}'
-
-#         elif event['httpMethod'] == 'DELETE' and event['resource'] == '/members/{player}':
-#             # add or update a member
-#             player = event['pathParameters']['player']
-#             timestamp = datetime.today().strftime('%Y%m%d%H%M%S')
-
-#             item = {
-#                 'invasion': f'#memberevent',
-#                 'id': timestamp,
-#                 'event': "delete",
-#                 'player': player
-#             }
-
-#             response = table.delete_item(Key={'invasion': f'#member', 'id': player}, ReturnValues='ALL_OLD')
-#             if 'Attributes' in response:
-#                 body = f'Deleted member {player}: {response["Attributes"]}'
-#                 table.put_item(Item=item)
-#             else:
-#                 body = f'Member {player} not found, nothing to delete'
-
-#         else:
-#             print(f"Unsupported route: {event['httpMethod']} {event['resource']}")
-#             statusCode = 400
-#             body = f"Unsupported route: {event['httpMethod']} {event['resource']}"
-
-#     except Exception as e:
-#         print(e)
-#         raise
-
-#     return {
-#         'statusCode': statusCode,
-#         'headers': headers,
-#         'body': json.dumps(body)
-#     }
-
-
--- invasions/src/bot/bot_reports.py
+++ invasions/src/bot/bot_reports.py
@@ -1,4 +1,3 @@
-import json
 import boto3
 from botocore.exceptions import ClientError
 from boto3.dynamodb.conditions import Key, Attr
