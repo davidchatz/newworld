1415e97e94d6ac8cb9d317c2e54427e7ef9c8548
Chatz <david.chatterton@gmail.com>
2024-06-30 13:50:06 +1000

    Improve process tests for large image

--- invasions/tests/test_process.py
+++ invasions/tests/test_process.py
@@ -132,6 +132,54 @@ def download_sample_jpg_file():
         'Quiet': True
     })
 
+@pytest.fixture
+def generate_invasion_ladders():
+    invasion = IrusInvasion.from_user(day=11, month=6, year=2024, settlement='rw', win=True)
+    logger.debug(f'Invasion {invasion}')
+
+    Chatz01 = IrusMember.from_user(player = "Chatz01", day=1, month=5, year=2024, faction= "purple", admin=False, salary=True)
+    Stuggy = IrusMember.from_user(player = "Stuggy", day=1, month=5, year=2024, faction= "green", admin=True, salary=True)
+    Talimonk = IrusMember.from_user(player = "Talimonk", day=1, month=5, year=2024, faction= "yellow", admin=False, salary=True)
+    kbaz = IrusMember.from_user(player = "kbaz", day=1, month=5, year=2024, faction= "purple", admin=False, salary=False)
+    Merkavar = IrusMember.from_user(player = "Merkavar", day=1, month=5, year=2024, faction= "yellow", admin=False, salary=True)
+    Fred = IrusMember.from_user(player = "Fred", day=1, month=5, year=2024, faction= "yellow", admin=False, salary=True)
+
+    for f in ['Screenshot_2024-05-23_222523.png', 'Screenshot_2024-05-23_222537.png', 'Screenshot_2024-05-23_222607.png', 'Screenshot_2024-05-23_222625.png', 'Screenshot_2024-05-23_222639.png', 'Screenshot_2024-05-23_222655.png', 'Screenshot_2024-05-23_222705.png']:
+        url = test_bucket.meta.client.generate_presigned_url(
+            'get_object',
+            Params={
+                'Bucket': test_bucket_name,
+                'Key': f'20240523-wf/{f}'
+            })
+        logger.debug(f'URL: {url}')
+
+        event = {
+            'invasion': invasion.name,
+            'filename': f'{f}',
+            'url': url,
+            'folder': invasion.path_ladders(),
+            'process': 'ladder'
+        }
+        logger.debug(f'Event: {event}')
+
+        result = client.lambda_client.invoke(FunctionName='Process',
+                                            InvocationType='RequestResponse',
+                                            Payload=json.dumps(event))
+        logger.debug(f'Result: {result}')
+
+        response = json.loads(result['Payload'].read())
+        logger.info(f'Result payload: {response}')
+
+    ladders = IrusLadder.from_invasion(invasion)
+    yield ladders
+    invasion.delete_from_table()
+    Chatz01.remove()
+    Stuggy.remove()
+    Talimonk.remove()
+    kbaz.remove()
+    Merkavar.remove()
+    Fred.remove()
+
 
 def test_download_sample_ladder_file(download_sample_ladder_file):
     assert download_sample_ladder_file['statusCode'] == 200
@@ -145,4 +193,10 @@ def test_download_sample_jpg_file(download_sample_jpg_file):
     logger.info(f'Result: {download_sample_jpg_file}')
     assert download_sample_jpg_file['statusCode'] == 400
 
-
+def test_generate_invasion_ladders(generate_invasion_ladders):
+    assert generate_invasion_ladders is not None
+    assert generate_invasion_ladders.invasion is not None
+    logger.info(generate_invasion_ladders.csv())
+    assert generate_invasion_ladders.count() == 52
+    assert generate_invasion_ladders.members() == 5
+    assert generate_invasion_ladders.is_contiguous_from_1() == True
